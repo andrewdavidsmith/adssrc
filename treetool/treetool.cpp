6b2be0a02df1bb7b3eceaeb9657daba9f1a8a00d
Andrew Smith <andrew@smithlabresearch.org>
2014-06-01 20:13:54 -0700

    Adding more code... a work in progress

--- treetool/treetool.cpp
+++ treetool/treetool.cpp
@@ -57,6 +57,7 @@
 #include <fstream>
 #include <algorithm>
 #include <numeric>
+#include <sstream>
 
 #include <cctype> // for isspace
 
@@ -65,11 +66,11 @@
 #include "smithlab_utils.hpp"
 #include "smithlab_os.hpp"
 
-
 using std::string;
 using std::vector;
 using std::endl;
 using std::cerr;
+using std::cout;
 
 
 static bool
@@ -92,7 +93,7 @@ public:
   bool has_children() const {return !child.empty();}
   bool is_leaf() const {return child.empty();}
   
-  string tostring() const;
+  string tostring(const size_t depth = 0) const;
 
 private:
   vector<PhyloTreeNode> child;
@@ -101,35 +102,77 @@ private:
   
 };
 
+string
+PhyloTreeNode::tostring(const size_t depth) const {
+  std::ostringstream oss;
+  oss << string(depth, '\t') << name << ':' << branch_length;
+  for (size_t i = 0; i < child.size(); ++i)
+    oss << endl << child[i].tostring(depth + 1);
+  return oss.str();
+}
+
 
 static bool
 represents_leaf(const string &tree_rep) {
-  return true;
+  return (tree_rep.find_first_of(',') == string::npos);
 }
 
 
 static string
 extract_name(const string &tree_rep) {
-  return string();
+  const size_t final_parenthesis = tree_rep.find_last_of(")");
+  const size_t possible_name_start = 
+    (final_parenthesis == string::npos) ? 0 : final_parenthesis + 1;
+  const size_t branch_len_start = 
+    std::min(tree_rep.find_first_of(":", possible_name_start), 
+	     tree_rep.length());
+  return string(tree_rep.begin() + possible_name_start,
+		tree_rep.begin() + branch_len_start);
 }
 
 
 static double
 extract_branch_length(const string &tree_rep) {
-  return 0.0;
+  const size_t final_parenthesis = tree_rep.find_last_of(")");
+  const size_t colon_pos = tree_rep.find_first_of(":", final_parenthesis + 1);
+  if (colon_pos == string::npos)
+    return 0.0;
+  const size_t non_numeric = 
+    tree_rep.find_first_not_of("0123456789.", colon_pos + 1);
+  if (non_numeric == 0)
+    return 0.0;
+  return atof(tree_rep.substr(colon_pos + 1).c_str());
 }
 
 
 static bool
 root_has_name(const string &tree_rep) {
-  return true;
+  const size_t final_parenthesis = tree_rep.find_last_of(")");
+  const size_t possible_name_start = 
+    (final_parenthesis == string::npos) ? 0 : final_parenthesis;
+  return (tree_rep[possible_name_start] != ':');
 }
 
 
 static void
 extract_subtrees(const string &tree_rep,
-		 vector<string> &sub_subtree_reps) {
-  return;
+		 vector<string> &subtree_reps) {
+  const size_t offset = (tree_rep[0] == '(') ? 1 : 0;
+  const string tmp(tree_rep.begin() + offset, 
+		   tree_rep.begin() + tree_rep.find_last_of(")"));
+  vector<size_t> split_points;
+  size_t p_count = 0;
+  for (size_t i = 0; i < tmp.length(); ++i) {
+    if (p_count == 0 && tmp[i] == ',')
+      split_points.push_back(i);
+    if (tmp[i] == '(') ++p_count;
+    if (tmp[i] == ')') --p_count;
+  }
+  split_points.push_back(tmp.length());
+  subtree_reps.push_back(tmp.substr(0, split_points[0]));
+  for (size_t i = 0; i < split_points.size() - 1; ++i)
+    subtree_reps.push_back(string(tmp.begin() + split_points[i] + 1,
+				  tmp.begin() + split_points[i + 1]));
 }
 
 
@@ -140,7 +183,7 @@ PhyloTreeNode::PhyloTreeNode(const string &tree_rep) {
   branch_length = extract_branch_length(tree_rep);
   if (root_has_name(tree_rep))
     name = extract_name(tree_rep);
-
+  
   if (!represents_leaf(tree_rep)) {
     vector<string> subtree_reps;
     extract_subtrees(tree_rep, subtree_reps);
@@ -161,9 +204,30 @@ public:
     tree_rep.erase(--w, tree_rep.end()); // The "--" is for the ";"
     root = PhyloTreeNode(tree_rep);
   }
+  string tostring() const {return root.tostring();}
+
 private:
   PhyloTreeNode root;
 };
+
+
+static string
+read_tree_from_file(const string &newick_file) {
+  std::ifstream in(newick_file.c_str());
+  if (!in)
+    throw SMITHLABException("bad file: " + newick_file);
+  
+  /* doing it this way to just get one tree */
+  string r;
+  char c;
+  bool found_end = false;
+  while (in >> c && !found_end) {
+    r += c;
+    if (c == ';')
+      found_end = true;
+  }
+  return r;
+}
 
 
 int 
@@ -201,7 +265,15 @@ main(int argc, const char **argv) {
     }
     const string newick_file = leftover_args.front();
     /****************** END COMMAND LINE OPTIONS *****************/
+
+    const string tree_rep(read_tree_from_file(newick_file));
     
+    if (VERBOSE)
+      cerr << tree_rep << endl;
+    
+    const PhyloTree t(tree_rep);
+    
+    cout << t.tostring() << endl;
     
   }
   catch (const SMITHLABException &e) {
